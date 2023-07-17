6134181ba0aa55b2644cda173aee9a1f94bdeac4
JessicaUpani-DCI <jessica.upani@digitalcareerinstitute.org>
2023-07-17 19:17:21 +0200

    changes to settings

--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('This is the home page!')
+    return render(request, 'index.html', {'movies': ['Designated Survivor', 'London Has Fallen', 'Avator']})
 
 def movies(request):
     return render(request, 'movies/movies.html', {'movies': ['movie1', 'movie2', 'movie3']})
